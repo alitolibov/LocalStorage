4af8753bbfa35f9efbbbcaf576f5851c70de2512
alitolibov <109537573+alitolibov@users.noreply.github.com>
2022-10-17 18:03:14 +0500

    Add files via upload

---
+++ registration/script.js
@@ -0,0 +1,25 @@
+let form = document.forms.form
+let data = localStorage.getItem('user')
+let text = document.querySelector('.text2')
+form.onsubmit = (e) => {
+    e.preventDefault()
+    let arr = {
+        id: Math.random()
+    }
+
+    let fm = new FormData(form)
+
+    fm.forEach((value,key) => {
+        arr[key] = value
+    })
+
+    localStorage.setItem('user', JSON.stringify(arr))
+}
+
+    if(data) {
+        window.location.assign("../log/index.html")
+    }
+    console.log(data);
+    text.onclick = () => {
+        window.location.assign("../log/index.html")
+    }
